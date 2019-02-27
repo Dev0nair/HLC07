731be496e48ee3a1dc5311cc04d3563eb93c90f2
Ismael Gonzalez Risco <40613999+Dev0nair@users.noreply.github.com>
2019-02-27 19:37:23 +0100

    sdf

---
+++ HLC07-Ismael/src/java/db/TipoProducto.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+/**
+ *
+ * @author alumno
+ */
+public class TipoProducto {
+    private int id;
+    private int id_empleado;
+    private String descripcion;
+
+    public TipoProducto(int id, int id_empleado, String descripcion) {
+        this.id = id;
+        this.id_empleado = id_empleado;
+        this.descripcion = descripcion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_empleado() {
+        return id_empleado;
+    }
+
+    public void setId_empleado(int id_empleado) {
+        this.id_empleado = id_empleado;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+}
